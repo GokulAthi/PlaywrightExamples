f6ed3acf7152d65df1ffc1620faa41daba36e58c
GokulAthi <goku2812@gmail.com>
2022-07-24 14:26:21 +0530

    Playwright sample program

---
+++ PlaywrightExamples/PlaywrightIntroSampleProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PlaywrightExamples
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+
+            var page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://www.google.com");
+        }
+    }
+}
